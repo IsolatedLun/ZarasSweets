7b5e80b2025933a77538911a54583aef7819bd6f
unknown <herojoe210@gmail.com>
2023-11-19 19:52:32 +0200

    Added redirecting for in app social media browsers to actual browsers

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
+	import { onMount } from "svelte";
 	import Footer from "../components/Layouts/Footer/Footer.svelte";
 	import Modal from "../components/Layouts/Modal/Modal.svelte";
     import Navbar from "../components/Layouts/Navbar/Navbar.svelte";
+	import { handleInAppUsers } from "../utils/general";
+
+	onMount(() => {
+		handleInAppUsers();
+	})
 
 </script>
 
--- frontend/src/utils/general.ts
+++ frontend/src/utils/general.ts
@@ -5,3 +5,17 @@ export function capitalize(x: string) {
 export function deCleanText(x: string) {
     return x.split(' ').map(word => word.toLowerCase()).join("_")
 }
+
+export function handleInAppUsers() {
+    const userAgent = window.navigator.userAgent;
+    if(!userAgent.includes("Instagram") || !userAgent.includes("Facebook")) {
+        return;
+    }
+
+    if(userAgent.includes("Iphone") || userAgent.includes("Ipad") || userAgent.includes("Ipod")) {
+        window.location.href = "safari://" + window.location.href;
+    } else {
+        window.location.href = "googlechrome://" + window.location.href;
+    }
+
+}
